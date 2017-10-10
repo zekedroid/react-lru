cd610091381a7050a1e26083efd414e452f9dc92
Zeke Schmois <zeke.schmois@novartis.com>
2017-10-10 09:13:33 -0400

    add bundling file export to window variable

---
+++ src/index_bundle.js
@@ -0,0 +1,19 @@
+import renderReactCell from './renderReactCell';
+import MemoizedReactDomContainers from './MemoizedReactDomContainers';
+import SimpleMemoizedReactDomContainers from './SimpleMemoizedReactDomContainers';
+
+window.renderReactCell = renderReactCell;
+window.MemoizedReactDomContainers = MemoizedReactDomContainers;
+window.SimpleMemoizedReactDomContainers = SimpleMemoizedReactDomContainers;
+
+export {
+    renderReactCell,
+    MemoizedReactDomContainers,
+    SimpleMemoizedReactDomContainers,
+}
+
+export default {
+    renderReactCell,
+    MemoizedReactDomContainers,
+    SimpleMemoizedReactDomContainers,
+}
